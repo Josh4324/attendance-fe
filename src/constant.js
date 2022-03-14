436d5e574555bdac4b009205ca304401f1b8672d
Josh <adesanyajoshua@ymail.com>
2022-03-14 11:14:20 +0100

    build

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,5 @@
-export const http = "http://localhost:8080";
-//export const http = "http://139.59.162.67";
+//export const http = "http://localhost:8080";
+export const http = "http://139.59.162.67";
 
 //export const front = "http://localhost:3001";
 //export const front = "139.59.162.67;
